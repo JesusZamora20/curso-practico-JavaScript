13b7d41e632a8a99c5dcc4eb845c2d928d162924
Jesus Zamora <jbermudezamora87@gmail.com>
2022-05-09 21:13:45 -0500

    se encapsulo el codigo en funciones para poder pasar cualquier argumento

--- figuras.js
+++ figuras.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+//CUADRADO
+console.group("Cuadrado");
+
+function PerimetroCuadrado(lado){
+    return lado * 4;
+}
+
+function AreaCuadrado(lado){
+    return lado * lado;
+}
+
+console.groupEnd();
+
+//TRIANGULO
+console.group("Triangulo");
+
+function PerimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
+
+function AreaTriangulo(base, altura){
+    return (base * altura)/2
+}
+console.groupEnd();
+
+//CIRCULO
+console.log("Circulo");
+
+function PerimetroCirculo(radio){
+    return radio * Math.PI * 2;
+}
+
+function AreaCirculo(radio){
+    return radio * radio * Math.PI;
+}
+
+console.groupEnd();
