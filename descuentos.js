0baf39798b0a89ffeab51b55f76930f5fc5777c5
Jesus Zamora <jbermudezamora87@gmail.com>
2022-05-18 20:58:39 -0500

    Taller 2 terminado, y reto de cupones completo

--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,8 @@
+const coupons = [
+    "Jesus_es_spiderman",
+    "nadie_lo_sabe"
+];
+
 function CalcularPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioDescuento = (precio * porcentajePrecioConDescuento)/100;
@@ -9,10 +14,22 @@ function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
 
-    const precioConDescuento = CalcularPrecioConDescuento(priceValue, discountValue);
+    let descuento;
+
+    switch(couponValue){
+        case coupons[0]:
+            descuento = 15;
+            break;
+        
+        case coupons[1]:
+            descuento = 30;
+            break;
+    }
+
+    const precioConDescuento = CalcularPrecioConDescuento(priceValue, descuento);
 
     const resultPrice = document.getElementById("ResultPrice");
     resultPrice.innerText = "El precio con descuento son: $" + precioConDescuento;
