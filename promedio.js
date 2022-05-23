48c003782a0644c95e7897cfa1d8707d2ed03358
Jesus Zamora <jbermudezamora87@gmail.com>
2022-05-22 22:15:33 -0500

    se realizó el ejercicio de integrar JS con HTML para el calculo del promedio

--- promedio.js
+++ promedio.js
@@ -1,22 +1,31 @@
-const lista1 = [
-    100,200,300,500
-];
+let listaPromedio = [];
+
+function AddNumber(){
+    let numberToAdd = document.getElementById("numberToAdd");
+    let newNumber = Number(numberToAdd.value);
+
+    listaPromedio.push(newNumber);
 
 
+    const showList = document.getElementById("list");
+    showList.innerText = listaPromedio;
 
-
-function CalcularPromedio(lista){
-    /*let  sumaLista =0;
-
-    for(let i = 0;i < lista.length; i++){
-        sumaLista = sumaLista + lista[i];
-    }*/
-
-    const sumaLista = lista.reduce(
-        (valorAcumulado = 0,nuevoElemento)=> valorAcumulado + nuevoElemento
-    );
-
-    const result = sumaLista/lista.length;
-    return result;
+    document.getElementById('numberToAdd').value= null;
 
 }
+
+function CalcularPromedio(){
+
+    const sumaLista = listaPromedio.reduce(
+        function (valorAcumulado = 0, nuevoElemento) {
+          return valorAcumulado + nuevoElemento;
+        }
+      );
+    promedioLista = Number(sumaLista/listaPromedio.length);  
+
+    const result = document.getElementById("promedio");
+    result.innerText = `el promedio es ${promedioLista}`;
+}
+
+
+
